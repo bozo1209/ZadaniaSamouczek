ca35ba5fae34d0bb81e1f03c4ab7dd149def3312
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-01-30 14:20:10 +0100

    gggg

--- src/Metody.java
+++ src/Metody.java
@@ -1,10 +1,15 @@
 public class Metody {
     public static void main(String[] args) {
-        
+
         System.out.println(zwracaWiek());
+        System.out.println(zwracaImie());
     }
 
     public static int zwracaWiek(){
         return 30;
+    }
+
+    public static String zwracaImie(){
+        return "bozo";
     }
 }
