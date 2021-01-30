bf4fe14d680b33f1d333112889776d5273c5aa1c
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-01-30 20:26:07 +0100

    dodano zad 4, 5, 6

--- src/Metody.java
+++ src/Metody.java
@@ -4,6 +4,9 @@ public class Metody {
         System.out.println(zwracaWiek());
         System.out.println(zwracaImie());
         wypiszSumeRozniceIloczyn(5,5);
+        System.out.println(czyParzysta(3));
+        System.out.println(czyPodzielna(15));
+        System.out.println(doTrzeciej(3));
     }
 
     public static int zwracaWiek(){
@@ -18,5 +21,18 @@ public class Metody {
         System.out.println("suma " + (a+b));
         System.out.println("roznica " + (a-b));
         System.out.println("iloczyn " + (a*b));
+    }
+
+    public static boolean czyParzysta(int a){
+        return a % 2 == 0;
+    }
+
+    public static boolean czyPodzielna(int a){
+        return a % 3 == 0 && a % 5 == 0;
+    }
+
+    public static int doTrzeciej(int a){
+        double b = a;
+        return (int) Math.pow(b, 3.0);
     }
 }
