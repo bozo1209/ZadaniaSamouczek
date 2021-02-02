51503f6f9c84d7f9a4aff5ea2fc285ee680cb1be
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-02-02 21:34:54 +0100

    dodanie treści zadań i zadania 1 i 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Human human = new Human("Stefan", "m", 50, 174, 88);
+        System.out.println(human.getImie());
+        System.out.println(human.getWaga());
 
+        Prostokat prostokat = new Prostokat(10,4);
+        System.out.println(prostokat.przekatna());
     }
 }
