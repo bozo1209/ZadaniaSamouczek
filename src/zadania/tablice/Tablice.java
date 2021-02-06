e4487956c00f09804df5a57e75919eb96ba405e0
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-02-06 19:52:29 +0100

    dodanie zadania 2 i testu dla zadania 2

--- src/zadania/tablice/Tablice.java
+++ src/zadania/tablice/Tablice.java
@@ -12,4 +12,12 @@ public class Tablice {
         return new String[]{"a", "b", "c", "d", "e"};
     }
 
+//    zad 2
+    public int[] odwrocenieKolejnosci(int[] a){
+        int[] wynik = new int[a.length];
+        for (int i = 0; i < a.length; i++){
+            wynik[i] = a[a.length - 1 - i];
+        }
+        return wynik;
+    }
 }
