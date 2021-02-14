d1395d50472d0f03d52e2748531049f7d9769501
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-02-14 15:45:50 +0100

    dodanie zadania 9, 10 + test do 10

--- src/zadania/petle/Main.java
+++ src/zadania/petle/Main.java
@@ -1,12 +1,12 @@
 package zadania.petle;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
         Petle petle = new Petle();
         ArrayFactory arrayFactory = new ArrayFactory(5);
+        int[] tablicaInt = new int[]{9,8,7,6,5};
 
         petle.odZeraDoX(5);
         petle.odZeraDoX2(5);
@@ -14,5 +14,8 @@ public class Main {
         petle.wypisujeLiczbeOdOstatniejCyfry(12345678987654321L);
 
         arrayFactory.showMacierzJednostkowa();
+
+        System.out.println(petle.wyswietlenie(tablicaInt));
+
     }
 }
