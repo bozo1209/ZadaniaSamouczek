40683ddf3799decefb9d496f85300cf59d0b1068
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-02-17 22:30:48 +0100

    dodano typy proste, zad 3

--- src/zadania/typyProste/TypyProste.java
+++ src/zadania/typyProste/TypyProste.java
@@ -2,6 +2,7 @@ package zadania.typyProste;
 
 import zadania.petle.Petle;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TypyProste {
@@ -15,6 +16,28 @@ public class TypyProste {
         long x = scanner.nextLong();
         System.out.println("Twoja liczba: " + x + " w postaci binarnej: ");
         System.out.println(petle.przeksztalcanieDoFormyBinarnej(x));
+    }
+
+//    zad 3
+    private int pobieranieRozmiaruTablicy(){
+        System.out.println("podaj rozmiar tablicy: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    private int[] pobieranieElementowTablicy(){
+        int rozmiarTablicy = pobieranieRozmiaruTablicy();
+        int[] tablica = new int[rozmiarTablicy];
+        for (int i = 0; i < rozmiarTablicy; i++){
+            System.out.println("podaj element " + (i + 1) + " z " + rozmiarTablicy + ": ");
+            tablica[i] = new Scanner(System.in).nextInt();
+        }
+        return tablica;
+    }
+
+    public void wyswietlanieTablicyOdNajmniejszejDoNajwiekszej(){
+        Petle petle = new Petle();
+        int[] tablicaDoWyswietlenia = petle.sortowanieOdNajmniejszejDoNajwiekszej(pobieranieElementowTablicy());
+        System.out.println(Arrays.toString(tablicaDoWyswietlenia));
     }
 }
 
